f3015e3ac90252d4f3de9df84000e6852e81f68d
Madalin Turluianu <turluianu.petre.madalin@gmail.com>
2023-05-11 13:56:20 +0300

    .

---
+++ src/components/TestComponent/components/TestChildComponent/TestChildComponent.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native';
+
+export function TestChildComponent() {
+  return (
+    <View>
+      <Text>TestChildComponent</Text>
+    </View>
+  );
+}
---
+++ src/components/TestComponent/components/TestChildComponent/index.ts
@@ -0,0 +1,1 @@
+export { TestChildComponent } from './TestChildComponent';
