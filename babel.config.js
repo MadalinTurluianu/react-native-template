f3e0944936dcfe6529db34db1145ddac0a1f1ad7
Madalin Turluianu <turluianu.petre.madalin@gmail.com>
2023-05-11 10:22:27 +0300

    added aliases

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,33 @@
-module.exports = function(api) {
+const config = require("./tsconfig.json");
+
+const { baseUrl, paths } = config.compilerOptions;
+
+function getAliases() {
+  return Object.entries(paths).reduce((aliases, alias) => {
+    const key = alias[0].replace("/*", "");
+    const value = `${baseUrl}/${alias[1][0].replace("/*", "")}`;
+
+    return {
+      ...aliases,
+      [key]: value,
+    };
+  }, {});
+}
+
+const aliases = getAliases();
+
+module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
+    presets: ["babel-preset-expo"],
+    plugins: [
+      [
+        "module-resolver",
+        {
+          alias: aliases,
+          extensions: [".js", ".jsx", ".ts", ".tsx"],
+        },
+      ],
+    ],
   };
 };
---
+++ src/components/TestComponent/TestComponent.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+export function TestComponent() {
+  return (
+    <View>
+      <Text>TestComponent</Text>
+    </View>
+  );
+}
