2f781778c025ee542b8dcbe83fd6bed308c8aa50
Madalin Turluianu <turluianu.petre.madalin@gmail.com>
2023-05-11 11:20:27 +0300

    configured eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,28 @@
 module.exports = {
   root: true,
-  extends: ['@react-native-community', 'airbnb-typescript', 'prettier', 'prettier/@typescript-eslint', 'prettier/react'],
+  extends: ['plugin:import/recommended', 'airbnb-typescript', 'prettier'],
+  plugins: ['import', 'react', 'react-native', '@typescript-eslint', 'prettier', 'only-error'],
+  env: {
+    'react-native/react-native': true,
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    sourceType: 'module',
+    project: './tsconfig.json',
+  },
+  rules: {
+    'import/prefer-default-export': 0,
+    'react/jsx-filename-extension': [
+      2,
+      {
+        extensions: ['.jsx', '.tsx'],
+      },
+    ],
+  },
+  settings: {
+    'import/ignore': ['react-native'],
+  },
 };
--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
 import { TestComponent } from "@components/TestComponent";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, View } from "react-native";
 
 export default function App() {
   return (
